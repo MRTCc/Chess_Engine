6caac57908f986ad3d6cadb8f2ff764adc9e38ff
MRTCc <martuccifrance@gmail.com>
2020-10-26 17:18:04 +0100

    positional evaluation (not tested)

--- gametreesearching.py
+++ gametreesearching.py
@@ -2,6 +2,7 @@ import movemodule as mvm
 import piecesmodule as pcsm
 import threading
 import ctypes
+import evaluationmodule as evm
 import algebraicnotationmodule as algn
 from algebraicnotationmodule import (a8, b8, c8, d8, e8, f8, g8, h8,
                                      a7, b7, c7, d7, e7, f7, g7, h7,
@@ -320,149 +321,6 @@ class FenStrParser:
         return gameposition
 
 
-
-class Evaluator:
-    def __init__(self, listpiece, nummoves):
-        self.listpiece = listpiece
-        self.nummoves = nummoves
-        self.wkings = 0
-        self.bkings = 0
-        self.wqueens = 0
-        self.bqueens = 0
-        self.wrooks = 0
-        self.brooks = 0
-        self.wbishops = 0
-        self.bbishops = 0
-        self.wknights = 0
-        self.bknights = 0
-        self.wpawns = 0
-        self.bpawns = 0
-        self.wdoubledpawns = 0
-        self.bdoubledpawns = 0
-        self.wblockedpawns = 0
-        self.bblockedpawns = 0
-        self.wisolatedpawns = 0
-        self.bisolatedpawns = 0
-        self.mobility = 0
-        self._countpieces()
-        self._countdoubledpawns()
-        self._countblockedpawns()
-        self._countisolatedpawns(self.listpiece.whitepawns)
-        self._countisolatedpawns(self.listpiece.blackpawns)
-        self._countmobility(nummoves)
-
-    def _countpieces(self):
-        for piece in self.listpiece.whitepieces:
-            if isinstance(piece, pcsm.WhiteKing):
-                self.wkings += 1
-            elif isinstance(piece, pcsm.WhiteQueen):
-                self.wqueens += 1
-            elif isinstance(piece, pcsm.WhiteBishop):
-                self.wbishops += 1
-            elif isinstance(piece, pcsm.WhiteKnight):
-                self.wknights += 1
-            elif isinstance(piece, pcsm.WhiteRook):
-                self.wrooks += 1
-        for piece in self.listpiece.blackpieces:
-            if isinstance(piece, pcsm.BlackKing):
-                self.bkings += 1
-            elif isinstance(piece, pcsm.BlackQueen):
-                self.bqueens += 1
-            elif isinstance(piece, pcsm.BlackBishop):
-                self.bbishops += 1
-            elif isinstance(piece, pcsm.BlackKnight):
-                self.bknights += 1
-            elif isinstance(piece, pcsm.BlackRook):
-                self.brooks += 1
-        self.wpawns = len(self.listpiece.whitepawns)
-        self.bpawns = len(self.listpiece.blackpawns)
-
-    def _countdoubledpawns(self):
-        for column in algn.ranks:
-            columncount = 0
-            for pawn in self.listpiece.whitepawns:
-                if pawn.coordinate.isequalrank(column):
-                    columncount += 1
-            if columncount > 1:
-                self.wdoubledpawns += columncount
-        for column in algn.ranks:
-            columncount = 0
-            for pawn in self.listpiece.blackpawns:
-                if pawn.coordinate.isequalrank(column):
-                    columncount += 1
-            if columncount > 1:
-                self.bdoubledpawns += columncount
-
-    def _countblockedpawns(self):
-        for pawn in self.listpiece.whitepawns:
-            if not pawn.onestepmove():
-                self.wblockedpawns += 1
-        for pawn in self.listpiece.blackpawns:
-            if not pawn.onestepmove():
-                self.bblockedpawns += 1
-
-    def _countisolatedpawns(self, pawnslist):
-        for pawn in pawnslist:
-            issxisolated = True
-            isdxisolated = True
-            pawnrank = pawn.coordinate.getrank()
-            index = algn.ranks.index(pawnrank)
-            try:
-                sxrank = algn.ranks[index - 1]
-                for sxpawn in pawnslist:
-                    if sxpawn == pawn:
-                        continue
-                    if sxpawn.coordinate.isequalrank(sxrank):
-                        issxisolated = False
-            except IndexError:
-                pass
-            try:
-                dxrank = algn.ranks[index + 1]
-                for dxpawn in pawnslist:
-                    if dxpawn == pawn:
-                        continue
-                    if dxpawn.coordinate.isequalrank(dxrank):
-                        isdxisolated = False
-            except IndexError:
-                pass
-            if issxisolated and isdxisolated:
-                if pawnslist == self.listpiece.whitepawns:
-                    self.wisolatedpawns += 1
-                elif pawnslist == self.listpiece.blackpawns:
-                    self.bisolatedpawns += 1
-                else:
-                    raise AttributeError
-
-    def _countmobility(self, nummoves):
-        if nummoves > 5:
-            self.mobility = 1
-        else:
-            self.mobility = -1
-
-    def __call__(self):
-        pawnmaterial = (self.wpawns - self.bpawns) * 1
-        rookmaterial = (self.wrooks - self.brooks) * 5
-        knightmaterial = (self.wknights - self.bknights) * 3
-        bishopmaterial = (self.wbishops - self.bbishops) * 3
-        queenmaterial = (self.wqueens - self.bqueens) * 9
-        kingmaterial = (self.wkings - self.bkings) * 500
-        doubledpawns = (self.wdoubledpawns - self.bdoubledpawns) * 0.5
-        isolatedpawns = (self.wisolatedpawns - self.bisolatedpawns) * 0.5
-        blockedpawns = (self.wblockedpawns - self.bblockedpawns) * 0.5
-        mobility = self.mobility
-
-        self.evaluation = (pawnmaterial + rookmaterial + knightmaterial + bishopmaterial + queenmaterial + kingmaterial
-                           + doubledpawns + isolatedpawns + blockedpawns + mobility)
-        return self.evaluation
-
-    def __str__(self):
-        msg = "Evaluation of position: \n\t"
-        for key, value in self.__dict__.items():
-            msg += key + " --> "
-            msg += str(value) + "\n\t"
-        return msg
-
-
 # TODO Solo per debug
 testfile = open("built_tree_test", "w")
 
@@ -526,7 +384,7 @@ class GamePosition:
             self.value = 0
             return
         if curply >= maxply:
-            evaluator = Evaluator(self.listpiece, len(self.moves))
+            evaluator = evm.Evaluator(self.listpiece, len(self.moves))
             self.value = evaluator()
             return
         for move in self.moves:
@@ -616,7 +474,7 @@ if __name__ == '__main__':
     # u = UciMoveSetter(root, ['e1d1', 'e8d8'])
     # u()
     # print(pcsm.listpiece)
-
+    """"
     fen = FenStrParser('black')
     gameposition = fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
     setter = UciMoveSetter(gameposition, ['e2e4'])
@@ -627,3 +485,5 @@ if __name__ == '__main__':
     time.sleep(5)
     gamethread.killthread()
     print(gamethread.getbestmove())
+    """
+    
